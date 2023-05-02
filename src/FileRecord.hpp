05e305db3ab95fb0df733abdf7912da5d72ebe44
Evgeny Ezers <1596614-evezers@users.noreply.gitlab.com>
2023-05-02 12:10:27 +0300

    Create data structures

---
+++ src/FileRecord.hpp
@@ -0,0 +1,50 @@
+//
+// Created by evezers on 01/05/23.
+//
+
+#ifndef FS_FILERECORD_HPP
+#define FS_FILERECORD_HPP
+
+#include <cstdint>
+
+typedef uint16_t word;
+typedef uint8_t byte;
+
+enum RecordType : uint16_t {
+    TEMP_FILE = 0400,            // запись о временном файле,
+    FREE = 01000,                // запись о свободной области на носителе. Размер свободной области в блоках содержится в 5-ом слове, остальные слова не используются,
+    REGULAR_FILE = 02000,        // запись о постоянном файле,
+    REGULAR_PROTECTED = 0102000, // запись о постоянном файле, защищенном от операции удаления,
+    RECORDS_END = 04000,         // признак конца записей в данном сегменте.
+};
+
+union FSDate {
+    word dateWord;
+
+    class {
+    private:
+        byte Year : 4;
+
+    public:
+        byte Day : 4;
+        byte Month : 4;
+
+        [[nodiscard]] byte getYear() const{
+            return static_cast<byte>(Year + 1980);
+        }
+
+        void setYear(uint16_t newYear){
+            Year = (static_cast<byte>((newYear - 1980) & 0xF));
+        }
+    } parsedDate;
+};
+
+class FileRecord {
+public:
+    RecordType recordType = FREE; // 1 word
+    char fileName[10] = "12345.123"; // 5 words
+    uint16_t blockCount = 0xAAAA; // 1 word
+    FSDate date {.dateWord=0xBBBB }; //1 word
+};
+
+#endif //FS_FILERECORD_HPP
