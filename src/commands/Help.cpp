9686e7a76da25e67da2fa697f3a68d8a3ff92ba3
Evgeny Ezers <1596614-evezers@users.noreply.gitlab.com>
2023-05-18 11:55:50 +0300

    Move forEachHelp to UtilsFunctions

--- src/commands/Help.cpp
+++ src/commands/Help.cpp
@@ -1,7 +1,6 @@
 #include <sstream>
 
 #include "Help.h"
-#include "FSCommands.hpp"
 #include "UtilsFunctions.hpp"
 
 std::string Help::getQuery(){
@@ -13,33 +12,11 @@ std::string Help::checkAndAssemble(Parser &parser) {
     return ""; // always correct
 }
 
-template<std::size_t index, class ...Args>
-static typename std::enable_if<index == std::tuple_size_v<FSCommandsTuple>, std::string>::type
-forEachHelp([[maybe_unused]] Args... args){
-    return "";
-}
-
-template<std::size_t index = 0, class ...Args>
-static typename std::enable_if<index < std::tuple_size_v<FSCommandsTuple>, std::string>::type
-forEachHelp(Args... args){
-    using CommandClass = std::tuple_element_t<index, FSCommandsTuple>;
-
-    std::stringstream str;
-
-    auto command = std::make_shared<CommandClass>(args...);
-
-    str << "    " << CommandClass::getQuery() << " - " << CommandClass::description() << std::endl
-        << "        " << command->help() << std::endl
-        << forEachHelp<index + 1>(args...);
-
-    return str.str();
-}
-
 std::string Help::run() {
     std::stringstream str;
 
     str << "List of all supported commands: " << std::endl;
-    str << forEachHelp(&filesystem);
+    str << UtilsFunctions::forEachHelp(&filesystem);
 
     std::string string = str.str();
 
