b0660694a1431c52c51fca14e4aa5569b076fd00
Evgeny Ezers <1596614-evezers@users.noreply.gitlab.com>
2023-05-24 23:16:18 +0300

    Fix float validation in enter command

--- src/commands/Enter.cpp
+++ src/commands/Enter.cpp
@@ -36,7 +36,7 @@ std::string Enter::checkAmount(const Parser &parser) {
 std::string Enter::setLength(const keyArgs_t &keys) {
     if(auto it = keys.find("length"); it != keys.end() || ((it = keys.find("l")) != keys.end())) {
         // convert to int
-        if(MonCom::convertToNumber(it->second, length)) return LENGTH_CANT_CONVERT;
+        if(UtilsFunctions::convertToNumber(it->second, length)) return LENGTH_CANT_CONVERT;
         // check restrictions
         if(length < 1 || 65535 < length){
             return LENGTH_RESTRICTED;
